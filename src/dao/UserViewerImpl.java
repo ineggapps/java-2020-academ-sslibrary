30a554f6ca6c904d5ffd35654e736441d2ccfc0b
inegg <inegg.apps@gmail.com>
2020-02-27 08:15:44 +0900

    아이디 검색 기능 구현

--- src/dao/UserViewerImpl.java
+++ src/dao/UserViewerImpl.java
@@ -1,12 +1,14 @@
 package dao;
 
 import java.util.List;
+import java.util.Scanner;
 
-import db.LibraryStorage;
+import single.LibraryStorage;
 import vo.UserVO;
 
 public class UserViewerImpl implements UserViewer {
 
+	Scanner sc = new Scanner(System.in);
 	List<UserVO> list = LibraryStorage.getInstance().getUserList();
 
 	@Override
@@ -21,6 +23,20 @@ public class UserViewerImpl implements UserViewer {
 	@Override
 	public void findUserById() {
 		// 아이디로 검색하기
+		String id;
+		System.out.print("검색할 아이디 입력 > ");
+		id = sc.next();
+
+		System.out.println("===검색 결과===");
+		for (UserVO vo : list) {
+			if (id.equals(vo.getId())) {
+				System.out.println(vo);
+				return;
+			}
+		}
+
+		System.out.println("사용자를 찾을 수 없습니다.");
+
 	}
 
 }
