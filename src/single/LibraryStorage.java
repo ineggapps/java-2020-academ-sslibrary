8ad158d9906a44836e044b6c63b16ae1934dbb6a
inegg <inegg.apps@gmail.com>
2020-02-28 08:19:49 +0900

    정렬 메서드 추가

--- src/single/LibraryStorage.java
+++ src/single/LibraryStorage.java
@@ -1,9 +1,12 @@
 package single;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import vo.BookManageVO;
 import vo.BookVO;
@@ -16,17 +19,16 @@ public class LibraryStorage {
 
 	// 현재 세션
 	private UserVO loginMember = null;
-	
 
 	public UserVO getLoginMember() {
 		return loginMember;
 	}
 
-	public void setLoginMember(UserVO loginMember) {//수정
+	public void setLoginMember(UserVO loginMember) {// 수정
 		this.loginMember = loginMember;
 	}
 
-	public UserVO getUser(String id) {//유저 검색하여 가져오기 없으면 null 반환
+	public UserVO getUser(String id) {// 유저 검색하여 가져오기 없으면 null 반환
 		for (UserVO vo : userList) {
 			if (id.equals(vo.getId())) {
 				return vo;
@@ -34,8 +36,8 @@ public class LibraryStorage {
 		}
 		return null;
 	}
-	
-	public BookManageVO getBorrowUser(String id) {//유저 검색하여 가져오기 없으면 null 반환
+
+	public BookManageVO getBorrowUser(String id) {// 유저 검색하여 가져오기 없으면 null 반환
 		for (BookManageVO bmv : rentalList) {
 			if (id.equals(bmv.getId())) {
 				return bmv;
@@ -47,12 +49,23 @@ public class LibraryStorage {
 	// 회원 목록
 	private List<UserVO> userList = new ArrayList<>();
 	// 도서 목록
-	private Map<String, BookVO> bookList = new HashMap<>();
+	private Map<String, BookVO> bookList = new TreeMap<>();
 	// 대여-반납 목록
 	private List<BookManageVO> rentalList = new ArrayList<>();
 
 	public List<UserVO> getUserList() {
 		return userList;
+	}
+
+	// 유저 목록 이름순 정렬
+	public void sortByUserName() {
+		Comparator<UserVO> comparator = new Comparator<UserVO>() {
+			@Override
+			public int compare(UserVO o1, UserVO o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		};
+		Collections.sort(userList, comparator);
 	}
 
 	public Map<String, BookVO> getBookList() {
@@ -62,7 +75,7 @@ public class LibraryStorage {
 	public List<BookManageVO> getRentalList() {
 		return rentalList;
 	}
-	
+
 	public BookVO getBook(BookManageVO vo) {
 		return bookList.get(vo.getIsbn13());
 	}
