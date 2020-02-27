7df0d061802e1ad648d528d54b33b191cc65a49c
inegg <inegg.apps@gmail.com>
2020-02-27 16:25:06 +0900

    formatter

--- src/dao/UserManageImpl.java
+++ src/dao/UserManageImpl.java
@@ -8,54 +8,50 @@ import java.util.Scanner;
 import single.LibraryStorage;
 import vo.UserVO;
 
-
-public class UserManageImpl implements UserManage{
+public class UserManageImpl implements UserManage {
 	private Scanner sc = new Scanner(System.in);
-	
+
 	@Override
 	public void join() {
 		System.out.println("회원가입");
 		// id(키), pw, name, email
 		// id(키) 중복체크
-		
+
 		UserVO vo = new UserVO();
 		String id;
 		System.out.print("아이디 입력 >");
 		id = sc.next();
 		vo.setId(id);
-		
-		
-		if(LibraryStorage.getInstance().getUser(id)!=null) {
+
+		if (LibraryStorage.getInstance().getUser(id) != null) {
 			System.out.println("등록된 아이디 입니다 ㅠㅠ. \n");
 			return;
 		}
-		
+
 		try {
-			
+
 			System.out.print("이름 입력 >");
 			vo.setName(sc.next());
-			
-			/*	
-			System.out.print("아이디 입력 >");
-			vo.setId(sc.next());
-			*/
-			
+
+			/*
+			 * System.out.print("아이디 입력 >"); vo.setId(sc.next());
+			 */
+
 			System.out.print("비밀번호 입력 >");
 			vo.setPw(sc.next());
-			
+
 			System.out.print("이메일 입력 >");
 			vo.setEmail(sc.next());
-			
+
 			LibraryStorage.getInstance().getUserList().add(vo);
 			System.out.println("축> 회원가입이 완료 되었습니다. <하");
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Override
 	public void login() {
 		System.out.println("\n===로그인===");
@@ -94,65 +90,61 @@ public class UserManageImpl implements UserManage{
 		LibraryStorage.getInstance().setLoginMember(null);
 		System.out.println("안전하게 로그아웃 되었습니다.");
 	}
-	
-	
+
 	@Override
 	public void update() {
 		System.out.println("회원님의 정보 수정...\n");
-		
+
 		String id;
 		String pw;
-		
+
 		System.out.println("=========본인 확인 ========");
 		System.out.print("아이디 입력>");
 		id = sc.next();
-		
+
 		System.out.print("비밀번호 입력>");
 		pw = sc.next();
-		
+
 		UserVO vo = LibraryStorage.getInstance().getUser(id);
-		
-		if(vo==null || ! vo.getPw().equals(pw) || ! vo.getId().equals(id) ) {
+
+		if (vo == null || !vo.getPw().equals(pw) || !vo.getId().equals(id)) {
 			System.out.println("아이디와 비밀번호가 일치 하지 않아요 ㅠㅠ...\n");
 			return;
 		}
-		
+
 		System.out.print("수정할 비밀번호 >");
 		vo.setPw(sc.next());
-		
+
 		System.out.print("수정할 이름 >");
 		vo.setName(sc.next());
-		
+
 		System.out.print("수정할 이메일 >");
 		vo.setEmail(sc.next());
-		
-		System.out.println("수정된"+ vo.getName()+"님의 정보 : " + vo.toStringWithPassword());
-		System.out.println(vo.getName()+"님의 정보 수정이 완료 되었습니다.! \n");
+
+		System.out.println("수정된" + vo.getName() + "님의 정보 : " + vo.toStringWithPassword());
+		System.out.println(vo.getName() + "님의 정보 수정이 완료 되었습니다.! \n");
 	}
-	
-	
 
 	@Override
 	public void out() {
 		String id;
 		String pw;
-		
+
 		System.out.print("아이디 입력 >");
 		id = sc.next();
-		
+
 		System.out.print("비밀 번호 입력 >");
 		pw = sc.next();
-		
+
 		UserVO vo = LibraryStorage.getInstance().getUser(id);
-		if(vo==null || ! vo.getPw().equals(pw) || ! vo.getId().equals(id) ) {
+		if (vo == null || !vo.getPw().equals(pw) || !vo.getId().equals(id)) {
 			System.out.println("아이디와 비밀번호가 일치 하지 않아요 ㅠㅠ...\n");
 			return;
 		}
-		
+
 		LibraryStorage.getInstance().getUserList().remove(vo);
-		
-		System.out.println(vo.getName()+" 회원님 탈퇴가 되셨습니다...\n");
-		
+		System.out.println(vo.getName() + " 회원님 탈퇴가 되셨습니다...\n");
+		logout();
 	}
 
 }
