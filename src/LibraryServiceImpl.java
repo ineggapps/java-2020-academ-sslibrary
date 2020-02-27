3f647fdd59aac43b868968cbd0114abd96ee0930
inegg <inegg.apps@gmail.com>
2020-02-27 14:15:36 +0900

    도서 목록 출력 (임시 알고리즘)

--- src/LibraryServiceImpl.java
+++ src/LibraryServiceImpl.java
@@ -1,12 +1,16 @@
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 import dao.UserManageImpl;
 import dao.UserViewer;
 import single.LibraryStorage;
 import single.Services;
+import util.DateMaker;
+import vo.BookVO;
 import vo.UserVO;
 
 public class LibraryServiceImpl implements LibraryService {
@@ -14,8 +18,8 @@ public class LibraryServiceImpl implements LibraryService {
 	Scanner sc = new Scanner(System.in);
 	UserViewer uv = Services.getInstance().getUserViewer();
 	List<UserVO> userList = LibraryStorage.getInstance().getUserList();
-	UserManageImpl um = new UserManageImpl(); 
-	
+	UserManageImpl um = new UserManageImpl();
+
 	public void entrance() {
 		// 로그인 상태에 따라 메뉴 다르게 구성하기
 		boolean isGoing = true;
@@ -55,7 +59,7 @@ public class LibraryServiceImpl implements LibraryService {
 			um.login();
 			break;
 		case 2:// 회원가입
-			
+
 			um.join();
 			break;
 		}
@@ -104,7 +108,7 @@ public class LibraryServiceImpl implements LibraryService {
 	@Override
 	public void register() {
 		System.out.println("\n===회원가입===");
-		//join() 실행 
+		// join() 실행
 		System.out.println("회원가입 완료!");
 	}
 
@@ -116,6 +120,15 @@ public class LibraryServiceImpl implements LibraryService {
 		ch = sc.nextInt();
 		switch (ch) {
 		case 1:// 도서관리
+				// 임시 코드
+			Map<String, BookVO> books = LibraryStorage.getInstance().getBookList();
+			Set<String> isbn13Set = books.keySet();
+			System.out.println("=== 도서관 보관중인 서적 목록 === 총 " + isbn13Set.size() + "건");
+			Iterator<String> it = isbn13Set.iterator();
+			while (it.hasNext()) {
+				String isbn = it.next();
+				System.out.println(books.get(isbn));
+			}
 			break;
 		case 2:// 회원관리
 			System.out.print("1.회원목록 2.아이디검색 3.이전메뉴 > ");
@@ -181,8 +194,31 @@ public class LibraryServiceImpl implements LibraryService {
 		list.add(new UserVO("english2", "2222", "심우철", "woocheol@gmail.com"));
 		list.add(new UserVO("english3", "2222", "조태정", "taejeong@gmail.com"));
 		list.add(new UserVO("english4", "2222", "손진숙", "jinsuk@gmail.com"));
+
 		list.add(new UserVO("english5", "2222", "김기훈", "gihun@gmail.com"));
 		list.add(new UserVO("computer", "3333", "박미진", "mijin@gmail.com"));
+
+		DateMaker dm = new DateMaker();
+		Map<String, BookVO> bookList = LibraryStorage.getInstance().getBookList();
+		bookList.put("9788934900115", new BookVO("9788934900115", "지능의 함정", "데이비드 롭슨 저/이창신 역", "김영사",
+				dm.toDate("20200113"), randomNumber(), "인문"));
+		bookList.put("9791189995539", new BookVO("9791189995539", "우리가 인생이라 부르는 것들", "정재찬 저", "인플루엔셜",
+				dm.toDate("20200225"), randomNumber(), "인문"));
+		bookList.put("9791189584559", new BookVO("9791189584559", "죽은 철학자의 살아있는 인생수업", "시라토리 하루히코, 지지엔즈 저/김지윤 역",
+				"포레스트북스", dm.toDate("20200306"), randomNumber(), "인문"));
+		bookList.put("9791162242551", new BookVO("9791162242551", "개발 7년차, 매니저 1일차", "카미유 푸르니에/권원상, 한민주 역", "한빛미디어",
+				dm.toDate("20200204"), randomNumber(), "IT 모바일"));
+		bookList.put("9791160509762", new BookVO("9791160509762", "2020 시나공 정보처리기사 필기", "길벗알앤디 저", "길벗",
+				dm.toDate("20191118"), randomNumber(), "컴퓨터 수험서"));
+		bookList.put("9788952753946", new BookVO("9788952753946", "날씨가 좋으면 찾아가겠어요", "이도우 저", "시공사",
+				dm.toDate("20200201"), randomNumber(), "소설/시/희곡"));
+		bookList.put("9788936434267", new BookVO("9788936434267", "날씨가 좋으면 찾아가겠어요", "손원평 저", "창비",
+				dm.toDate("20170331"), randomNumber(), "소설/시/희곡"));
+
+	}
+
+	public int randomNumber() {
+		return (int) (Math.random() * 100);
 	}
 
 }
