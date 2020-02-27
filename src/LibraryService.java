5fea251c247403c0dde0afcaa188408f602998c7
inegg <inegg.apps@gmail.com>
2020-02-27 16:24:30 +0900

    인터페의스에서 로그인, 로그아웃, 회원가입 메서드 삭제 (UserManage에서 구현함)

--- src/LibraryService.java
+++ src/LibraryService.java
@@ -5,12 +5,6 @@ public interface LibraryService {
 
 	public void entrance(); // 로그인 상태에 따라 다른 화면 보여주기
 
-	public void login();
-
-	public void logout();
-
-	public void register();
-
 	public boolean showDefaultMenu();
 
 	public boolean showAdminMenu();
