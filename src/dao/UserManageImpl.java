1761fed88525e92e5f71c1466cf6c3fe792645da
inegg <inegg.apps@gmail.com>
2020-02-28 17:09:08 +0900

    남의 정보가 수정되는 현상도 수정하였음.

--- src/dao/UserManageImpl.java
+++ src/dao/UserManageImpl.java
@@ -45,8 +45,8 @@ public class UserManageImpl implements UserManage {
 			vo.setEmail(sc.next());
 
 			LibraryStorage ls = LibraryStorage.getInstance();
-			ls.getUserList().add(vo);//유저 추가
-			ls.sortByUserName();//정렬 호출
+			ls.getUserList().add(vo);// 유저 추가
+			ls.sortByUserName();// 정렬 호출
 			System.out.println("축> 회원가입이 완료 되었습니다. <하");
 
 		} catch (Exception e) {
@@ -79,38 +79,41 @@ public class UserManageImpl implements UserManage {
 			System.out.println("존재하지 않는 계정입니다.");
 			return;
 		} else if (!pw.equals(vo.getPw())) {
-			System.out.println("아이디와 비밀번호가 일치하지 않습니다."); //강사님이! 아이디나 비밀번호 틀렸을 땐 알려주면 안된다고 했던거 생각해서 수정했습니다!
+			System.out.println("아이디와 비밀번호가 일치하지 않습니다."); // 강사님이! 아이디나 비밀번호 틀렸을 땐 알려주면 안된다고 했던거 생각해서 수정했습니다!
 			return;
 		}
 
-		System.out.println(vo.getName() +"님이 로그인하셨습니다.");
+		System.out.println(vo.getName() + "님이 로그인하셨습니다.");
 		LibraryStorage.getInstance().setLoginMember(vo);
 	}
 
-	
 	@Override
 	public void logout() {
 		logout(false);
 	}
-	
+
 	public void logout(boolean isSilent) {
 		LibraryStorage.getInstance().setLoginMember(null);
-		if(!isSilent) {
+		if (!isSilent) {
 			System.out.println("안전하게 로그아웃 되었습니다.");
 		}
 	}
 
 	@Override
 	public void update() {
-		System.out.println("회원님의 정보 수정...\n");
+		System.out.println("== 정보 수정...==\n");
 
+		UserVO user = LibraryStorage.getInstance().getLoginMember();
+		if (user == null) {
+			System.out.println("로그인 상태가 아닙니다");
+			return;
+		}
 		String id;
 		String pw;
 
 		System.out.println("=========본인 확인 ========");
-		System.out.print("아이디 입력>");
-		id = sc.next();
-
+		System.out.printf("%s(%s)님의 정보 수정\n", user.getId(), user.getName());
+		id = user.getId();
 		System.out.print("비밀번호 입력>");
 		pw = sc.next();
 
@@ -154,14 +157,14 @@ public class UserManageImpl implements UserManage {
 		LibraryStorage.getInstance().getUserList().remove(vo);
 		List<BookManageVO> rentalList = LibraryStorage.getInstance().getRentalList();
 		List<BookManageVO> removeList = new ArrayList<>();
-		//강제 반납 대상 책 목록
+		// 강제 반납 대상 책 목록
 		for (BookManageVO bm : rentalList) {
 			if (bm.getId().equals(id)) {
 				removeList.add(bm);
 			}
 		}
-		//반납하기
-		//반납 후 대여일지에서 해당 회원목록을 삭제
+		// 반납하기
+		// 반납 후 대여일지에서 해당 회원목록을 삭제
 		BookTransaction bt = Services.getInstance().getBookTransaction();
 		for (BookManageVO bm : removeList) {
 			bt.returnBook(bm);
