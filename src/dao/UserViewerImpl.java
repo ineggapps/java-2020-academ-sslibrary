6783e8aa0fba2e51a07f169d9da7706bea7e5ddb
inegg <inegg.apps@gmail.com>
2020-02-29 08:43:12 +0900

    검색 알고리즘 수정 (equals => indexOf>=0)

--- src/dao/UserViewerImpl.java
+++ src/dao/UserViewerImpl.java
@@ -26,16 +26,20 @@ public class UserViewerImpl implements UserViewer {
 		String id;
 		System.out.print("검색할 아이디 입력 > ");
 		id = sc.next();
-
+		int cnt = 0;
 		System.out.println("===검색 결과===");
 		for (UserVO vo : list) {
-			if (id.equals(vo.getId())) {
+			if (vo.getId().indexOf(id) >= 0) {
 				System.out.println(vo);
-				return;
+				cnt++;
 			}
 		}
 
-		System.out.println("사용자를 찾을 수 없습니다.");
+		if (cnt > 0) {
+			System.out.println("==== " + cnt + "건의 사용자 정보를 찾았습니다. ====");
+		} else {
+			System.out.println("사용자를 찾을 수 없습니다.");
+		}
 
 	}
 
