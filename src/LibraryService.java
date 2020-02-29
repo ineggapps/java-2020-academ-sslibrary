a318eb7ee9ca5079a7f384d9e28ad8f2feb7d997
inegg <inegg.apps@gmail.com>
2020-02-29 09:24:39 +0900

    불필요한 상수 삭제

--- src/LibraryService.java
+++ src/LibraryService.java
@@ -1,7 +1,5 @@
 
 public interface LibraryService {
-	public static final String ADMIN_ID = "admin";
-	public static final String ADMIN_PW = "1234";
 
 	public void entrance(); // 로그인 상태에 따라 다른 화면 보여주기
 
